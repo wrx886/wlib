f4b69dcee4a255932926b1a00d55e67c6492749a
wrx886 <18750909886@139.com>
2025-10-01 21:36:53 +0800

    更新标识符拼接方案，修复数组中存在的问题

--- wlib.h
+++ wlib.h
@@ -25,6 +25,10 @@
 #define w_malloc(size) malloc(size)
 #define w_free(ptr) free(ptr)
 
+// 标识符拼接
+#define w_concat_(a, b) a##b
+#define w_concat(a, b) w_concat_(a, b)
+
 // ========================================================================================================================================================
 //  断言定义
 // ========================================================================================================================================================
@@ -43,129 +47,109 @@
     })
 
 // ========================================================================================================================================================
-//  数据类型需要支持的操作
-// ========================================================================================================================================================
-
-/**
- * 获取数据类型的哈希值计算函数
- * 函数原型：int64_t w_hash_##T(T this)
- * @param T 数据类型
- */
-#define w_hash(T) w_hash_##T
-
-/**
- * 获取数据类型的比较函数
- * 函数原型：bool w_equals_##T(T this, T other)
- * @param T 数据类型
- */
-#define w_equals(T) w_equals_##T
-
-// ========================================================================================================================================================
 //  数组
 // ========================================================================================================================================================
 
+// 数组类型
+#define w_Array(T) w_concat(w_Array_, T)
+
 // 数组类型定义
-#define w_Array_type_define_(T)                \
-    struct w_Array_##T                         \
-    {                                          \
-        T *data;      /* 存储数据的数组指针 */ \
-        int64_t size; /* 数组大小 */           \
-    };
+#define w_Array_type_define_(T)        \
+    typedef struct                     \
+    {                                  \
+        T *elementData; /* 元素数据 */ \
+        int size;       /* 元素数量 */ \
+    } w_Array(T);
 
-// 类型数组初始化函数定义
-#define w_Array_init_define_(T)                  \
-    /* 类型数组初始化函数 */                     \
-    static inline void w_Array_init_##T(         \
-        struct w_Array_##T *this,                \
-        int64_t size /* 数组大小 */)             \
-    {                                            \
-        w_assert(size >= 0);                     \
-        w_assert(this != NULL);                  \
-        this->data = w_malloc(size * sizeof(T)); \
-        memset(this->data, 0, size * sizeof(T)); \
-        this->size = size;                       \
+// 数组初始化
+#define w_Array_init(T) w_concat(w_Array(T), _init)
+#define w_Array_init_define_(T)                                         \
+    /**                                                                 \
+     * 数组初始化                                                  \
+     * @param this 数组                                               \
+     * @param size 数组大小                                         \
+     * @return void                                                     \
+     */                                                                 \
+    static inline void w_Array_init(T)(w_Array(T) * this, int64_t size) \
+    {                                                                   \
+        w_assert(this != NULL);                                         \
+        w_assert(size >= 0);                                            \
+        this->elementData = malloc(sizeof(T) * size);                   \
+        this->size = size;                                              \
     }
 
-// 类型数组反初始化函数定义
-#define w_Array_deinit_define_(T)                                   \
-    /* 类型数组反初始化函数 */                                      \
-    static inline void w_Array_deinit_##T(struct w_Array_##T *this) \
-    {                                                               \
-        w_assert(this != NULL);                                     \
-        w_assert(this->data != NULL);                               \
-        w_free(this->data);                                         \
-        this->data = NULL;                                          \
-        this->size = -1;                                            \
+// 数组反初始化
+#define w_Array_deinit(T) w_concat(w_Array(T), _deinit)
+#define w_Array_deinit_define_(T)                           \
+    /**                                                     \
+     * 数组反初始化                                   \
+     * @param this 数组                                   \
+     * @return void                                         \
+     */                                                     \
+    static inline void w_Array_deinit(T)(w_Array(T) * this) \
+    {                                                       \
+        w_assert(this != NULL);                             \
+        w_assert(this->elementData != NULL);                \
+        free(this->elementData);                            \
+        memset(this, 0, sizeof(w_Array(T)));                \
     }
 
-// 类型数组访问函数定义
-#define w_Array_get_define_(T)                                \
-    /*  类型数组访问函数 */                                   \
-    static inline T w_Array_get_##T(struct w_Array_##T *this, \
-                                    int64_t index /* 索引 */) \
-    {                                                         \
-        w_assert(this != NULL);                               \
-        w_assert(this->data != NULL);                         \
-        w_assert(index >= 0 && index < this->size);           \
-        return this->data[index];                             \
+// 获取数组元素
+#define w_Array_get(T) w_concat(w_Array(T), _get)
+#define w_Array_get_define_(T)                                       \
+    /**                                                              \
+     * 获取数组元素                                            \
+     * @param this 数组                                            \
+     * @param index 索引                                           \
+     * @return T 数组元素                                        \
+     */                                                              \
+    static inline T w_Array_get(T)(w_Array(T) * this, int64_t index) \
+    {                                                                \
+        w_assert(this != NULL);                                      \
+        w_assert(this->elementData != NULL);                         \
+        w_assert(index >= 0 && index < this->size);                  \
+        return this->elementData[index];                             \
     }
 
-// 类型数组修改函数定义
-#define w_Array_set_define_(T)                                   \
-    /*  类型数组修改函数 */                                      \
-    static inline void w_Array_set_##T(struct w_Array_##T *this, \
-                                       int64_t index, /* 索引 */ \
-                                       T value /* 新值 */)       \
-    {                                                            \
-        w_assert(this != NULL);                                  \
-        w_assert(this->data != NULL);                            \
-        w_assert(index >= 0 && index < this->size);              \
-        this->data[index] = value;                               \
+// 设置数组元素
+#define w_Array_set(T) w_concat(w_Array(T), _set)
+#define w_Array_set_define_(T)                                                 \
+    /**                                                                        \
+     * 设置数组元素                                                      \
+     * @param this 数组                                                      \
+     * @param index 索引                                                     \
+     * @param val 值                                                          \
+     * @return void                                                            \
+     */                                                                        \
+    static inline void w_Array_set(T)(w_Array(T) * this, int64_t index, T val) \
+    {                                                                          \
+        w_assert(this != NULL);                                                \
+        w_assert(this->elementData != NULL);                                   \
+        w_assert(index >= 0 && index < this->size);                            \
+        this->elementData[index] = val;                                        \
     }
 
-/**
- * 定义数组类型，在全局作用域（所有 {} 外）中使用，使用后才定义这个类型的数组
- * @param T 数组元素类型
- */
+// 数组大小
+#define w_Array_size(T) w_concat(w_Array(T), _size)
+#define w_Array_size_define_(T)                              \
+    /**                                                      \
+     * 获取数组大小                                    \
+     * @param this 数组                                    \
+     * @return int64_t 数组大小                          \
+     */                                                      \
+    static inline int64_t w_Array_size(T)(w_Array(T) * this) \
+    {                                                        \
+        w_assert(this != NULL);                              \
+        return this->size;                                   \
+    }
+
+// 数组定义
 #define w_Array_define(T)      \
     w_Array_type_define_(T);   \
     w_Array_init_define_(T);   \
     w_Array_deinit_define_(T); \
     w_Array_get_define_(T);    \
-    w_Array_set_define_(T);
-
-/**
- * 数组类型
- * @param T 数组元素类型
- */
-#define w_Array(T) struct w_Array_##T
-
-/**
- * 初始化数组，跳转到真实的初始化函数
- * 被调用函数原型：static inline void w_Array_init_##T(struct w_Array_##T *array, int64_t size)
- * @param T 数组类型
- */
-#define w_Array_init(T) w_Array_init_##T
-
-/**
- * 反初始化数组，跳转到真实的反初始化函数
- * 被调用函数原型：static inline void w_Array_deinit_##T(struct w_Array_##T *array)
- * @param T 数组类型
- */
-#define w_Array_deinit(T) w_Array_deinit_##T
-
-/**
- * 访问数组元素，跳转到真实的访问函数
- * 被调用函数原型：static inline T w_Array_get_##T(struct w_Array_##T *array, int64_t index)
- * @param T 数组类型
- */
-#define w_Array_get(T) w_Array_get_##T
-
-/**
- * 修改数组元素，跳转到真实的修改函数
- * 被调用函数原型：static inline void w_Array_set_##T(struct w_Array_##T *array, int64_t index, T value)
- * @param T 数组类型
- */
-#define w_Array_set(T) w_Array_set_##T
+    w_Array_set_define_(T);    \
+    w_Array_size_define_(T)
 
 #endif
